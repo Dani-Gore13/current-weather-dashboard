a3d87d567ff5750f435ee53ad00d982b64eca637
Dani-Gore13 <daningore11@gmail.com>
2023-05-06 14:46:56 -0400

    added updated code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,48 @@
-const apiKey = "API_KEY"; // replace with API key
-const city = "Richmond"; // replace with the name of the city you want to retrieve data for
+const API_KEY = "c54e427ce0efe00a4cc548351acba42e";
+const CITY_NAME = "Richmond";
+const WEATHER_URL = `https://api.openweathermap.org/data/2.5/forecast?q=${Richmond}&appid=${c54e427ce0efe00a4cc548351acba42e}`;
 
-// use the OpenWeatherMap API to retrieve weather data for the city
-fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
-  .then(response => response.json())
-  .then(data => {
-    console.log(data); // log the retrieved data to the console for debugging
+// Get a reference to the weather button and weather forecast div
+const weatherButton = document.getElementById("weatherButton");
+const weatherForecast = document.getElementById("weatherForecast");
 
-    // get the necessary data from the retrieved JSON object
-    const cityName = data.city.name;
-    const temperature = data.list[0].main.temp; 
-    const description = data.list[0].weather[0].description;
+// Add a click event listener to the weather button
+weatherButton.addEventListener("click", () => {
+  // Fetch weather data from OpenWeatherMap API
+  fetch(WEATHER_URL)
+    .then(response => response.json())
+    .then(data => {
+      // Get the list of weather forecasts for the next 5 days
+      const forecasts = data.list.filter(forecast => forecast.dt_txt.includes("12:00:00"));
 
-    // update the HTML elements in the weather dashboard with the retrieved data
-    document.getElementById("city-name").textContent = cityName;
-    document.getElementById("temperature").textContent = temperature;
-    document.getElementById("description").textContent = description;
-  })
-  .catch(error => console.error(error)); // log any errors to the console
+      // Clear any existing weather forecast elements
+      weatherForecast.innerHTML = "";
+
+      // Display the weather forecast for each day
+      forecasts.forEach(forecast => {
+        const date = new Date(forecast.dt_txt);
+        const temperature = Math.round(forecast.main.temp - 273.15); // Convert temperature from Kelvin to Celsius
+        const description = forecast.weather[0].description;
+
+        // Create HTML elements to display the weather forecast
+        const dayElement = document.createElement("div");
+        dayElement.classList.add("day");
+
+        const dateElement = document.createElement("div");
+        dateElement.textContent = date.toLocaleDateString("en-US", {weekday: "long"});
+
+        const temperatureElement = document.createElement("div");
+        temperatureElement.textContent = `${temperature}°C`;
+
+        const descriptionElement = document.createElement("div");
+        descriptionElement.textContent = description;
+
+        // Append the HTML elements to the weather forecast div
+        dayElement.appendChild(dateElement);
+        dayElement.appendChild(temperatureElement);
+        dayElement.appendChild(descriptionElement);
+        weatherForecast.appendChild(dayElement);
+      });
+    })
+    .catch(error => console.error(error));
+});
